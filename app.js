a2137cafe9c84a4b860b3806c2362cef2c5f9bd7
Adel Kuanysheva <akuanysheva@my.bcit.ca>
2021-03-16 11:17:30 -0400

    app.js added, since it was not pushed before'

---
+++ app.js
@@ -0,0 +1,23 @@
+
+let num1 = 5;
+let num2 = 10;
+const const1 = true;
+let string = 'Hello!';
+
+if (num1 === num2) {
+    console.log("they match") 
+} else {
+    console.log("no luck") 
+}
+
+/* test of the other condition:
+if (num1 === 5) {
+    console.log("they match") 
+} else {
+    console.log("no luck") 
+} 
+*/ 
+
+for (letter of string) {
+    console.log(letter) 
+} 
